8cc51e7017f13526fe120468075b4d1ba5054777
Maxim Volchinskiy <thyxaxa@gmail.com>
2021-12-04 18:26:13 +0200

    stopwatchRXJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+import {interval,Subject,} from 'rxjs';
+import {takeUntil,} from 'rxjs/operators';
+
 function App() {
+  const [time, setTime] = useState(0);
+  const [isTimerOn, setTimerOn] = useState(false);
+  const [startStopBtn, setStartStopBtn] = useState();
+
+  const handleClick = () => {
+    setTimerOn(!isTimerOn);
+  };
+
+  const handleWait = () => {
+    if (time !== 0) {
+      setTimerOn(false);
+    }
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    setTimerOn(false);
+  };
+
+  useEffect(() => {
+    if (isTimerOn) {
+      setStartStopBtn('Stop');
+    } else {
+      setStartStopBtn('Start');
+    }
+
+    const timeSubject$ = new Subject();
+    interval(1000)
+      .pipe(takeUntil(timeSubject$))
+      .subscribe(() => {
+        if (isTimerOn) {
+          setTime((val) => val + 1);
+        }
+      });
+
+    return () => {
+      timeSubject$.next();
+      timeSubject$.complete();
+    };
+  }, [isTimerOn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <div className="Stopwatch">
+          <div className="Stopwatch-left-side">
+            <div className="Display">
+              <div>{(time / 100) < 10 && <span>0</span>}{Math.trunc(time / 3600)}</div>
+
+              <div>:</div>
+
+              <div>{(time / 100) < 10 && <span>0</span>}{Math.trunc(time / 60) % 60}</div>
+
+              <div>:</div>
+
+              <div>{(time % 60) < 10 && <span>0</span>}{time % 60}</div>
+            </div>
+          </div>
+
+          <div className="Stopwatch-right-side">
+            
+              <div className="Btn-wrapper">
+                <button className={isTimerOn ? 'Stop-Btn' : ''} onClick={handleClick} id="start-stop" >{startStopBtn}</button>
+              </div>
+
+              <div className="Btn-wrapper">
+                <button className="Wait-Btn" onDoubleClick={handleWait} id="wait">Wait</button>
+              </div>
+
+              <div className="Btn-wrapper">
+                <button className="Reset-Btn" onClick={handleReset}>Reset</button>
+              </div>
+
+          </div>    
+        </div>
+    </section>
   );
 }
 
